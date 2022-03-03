e7fdcb10c100bcbcc7f36563528cd41ee01a4f36
Cloud User <centos@ip-172-31-1-4.ec2.internal>
2022-03-03 02:28:36 +0000

    Create links to main file

--- mutable/env-dev/vars.tf
+++ mutable/env-dev/vars.tf
@@ -1,10 +1,1 @@
-variable "OD_INSTANCE_COUNT" {}
-variable "SPOT_INSTANCE_COUNT" {}
-variable "OD_INSTANCE_TYPE" {}
-variable "SPOT_INSTANCE_TYPE" {}
-variable "COMPONENT" {}
-variable "ENV" {}
-variable "PORT" {}
-variable "TRIGGER" {
-  default = "no"
-}
+../vars.tf
